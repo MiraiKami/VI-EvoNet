7112d5232c24f66af48e4ec3def25e0ddcaaf35a
Quentin Leblanc <quentin.leblanc@hes-so.ch>
2021-12-17 11:48:07 +0100

    added home Component

--- frontend/src/component/homeComponent/homeComponent.js
+++ frontend/src/component/homeComponent/homeComponent.js
@@ -1,23 +1,47 @@
 import logo from "../../logo.svg";
 import React from "react";
+import {Button, Card, CardContent, Container, Grid, Paper} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import GovComponent from "../govComponent/govComponent";
+import OngComponent from "../ong-component/ong-component";
 
 export default function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="App" style={{marginTop: 150}}>
+            <Paper style={{height: "200vh"}}>
+                <Container>
+                <Grid container spacing={1}>
+                    <Grid item xs alignItems="center">
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Button
+                                    size="big"
+                                    variant="text"
+                                    component={Link}
+                                    to="/gov"
+                                    color="default">
+                                    Government
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs alignItems="center">
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <Button
+                                    size="big"
+                                    variant="text"
+                                    component={Link}
+                                    to="/ong"
+                                    color="default">
+                                    ONG
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    </Grid>
+                </Container>
+            </Paper>
         </div>
     );
 }
